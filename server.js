4187e0b9db8ce18507851c3a962d6744a92ead14
Trevor French <trevorf96@gmail.com>
2021-05-20 15:51:32 -0600

    swire bot hello world

--- server.js
+++ server.js
@@ -22,3 +22,17 @@ app.set('view engine', 'ejs');                                      // sets the 
 
 app.use(require('morgan')('combined'));                             // Use application-level middleware for common functionality, including
 app.use(bodyParser.urlencoded({ extended: true }));                 // logging, parsing, and session handling.
+
+app.set("port", (process.env.PORT || 5000));                        // sets the port to 5000
+app.use(express.static(path.join(__dirname, '')));                  // this allows js and css files to be linked to the HTML
+
+app.listen(app.get("port"), function () {                           // listens on the port and displays a message to the console
+	console.log("Now listening on port: " + app.get("port"));
+});
+
+app.get('/', 														// when the root directory loads, send the main.html file to the client
+	(req, res) =>
+		res.sendFile(
+			path.join(__dirname, 'index.html')
+		)
+	);
